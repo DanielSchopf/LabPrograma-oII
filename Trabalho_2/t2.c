89b2f6c7c62324518daaf3ef6c93f6eec6cad733
Daniel Schopf <74994656+DanielSchopf@users.noreply.github.com>
2023-04-13 15:06:43 -0300

    Update t2.c

--- Trabalho_2/t2.c
+++ Trabalho_2/t2.c
@@ -20,6 +20,12 @@ int** alocar_matriz(int n, int d) {
     return mat;
 }
 
+void palavra_maiuscula(char c){
+    for(int i=0; i<strlen(c);i++){
+        c[i] = toupper(c[i]);
+    }
+}
+
 void ler_mun(int n, Municipio *m, int d) {
     for (int i = 0; i < n; i++) {
         m[i].nome = (char *)malloc(100 + 1);
@@ -28,18 +34,54 @@ void ler_mun(int n, Municipio *m, int d) {
         m[i].nome[strcspn(m[i].nome, "\n")] = '\0'; // remove o caractere de nova linha
         printf("Digite a populacao de %s:\n", m[i].nome);
         scanf("%d", &m[i].populacao);
-        m[i].dados = alocar_matriz(n, d);
+        m[i].dados = alocar_matriz(n, 3);
+        for (int j = 0; j < d; j++) {
+            printf("Digite o dia:\n");
+            scanf("%d", &m[i].dados[j][0]);
+            printf("Digite a quantidade de casos confirmados no dia:\n");
+            scanf("%d", &m[i].dados[j][1]);
+            printf("Digite a quantidade de óbitos no dia:\n");
+            scanf("%d", &m[i].dados[j][2]);
+            system("clear");
+        }
         getchar();
     }
 }
 
+void imprime_mun(Municipio* m, int i, int d){
+        printf("Nome do município: %s\n", m[i].nome);
+        printf("População: %d\n", m[i].populacao);
+        printf("Dados:\n");
+        printf("DIAS\tCASOS CONFIRMADOS\tOBITOS\n");
+        for (int j = 0; j < d; j++) {
+            printf("%d\t\t%d\t\t%d\n", m[i].dados[j][0], m[i].dados[j][1], m[i].dados[j][2]);
+        }
+        return;
+
+}
+
+void busca_mun(int n, Municipio *m, int d) {
+    char c[100 + 1];
+    printf("Digite o nome do município:\n");
+    fgets(c, 100, stdin);
+    c[strcspn(c, "\n")] = '\0';
+    system("clear");
+    for (int i = 0; i < n; i++) {
+        if (strcmp(c, m[i].nome) == 0) {
+            imprime_mun(m,i,d);
+        } else {
+        printf("Município não encontrado.\n");
+        }
+    }
+
+}
 
 int main() {
     int n, d;
     printf("Digite o numero de municipios presentes na pesquisa:\n");
     scanf("%d", &n);
 
-    printf("Digite o numero de dias para a tabela de dados");
+    printf("Digite o numero de dias para a tabela de dados:\n");
     scanf("%d", &d);
 
     getchar();
@@ -50,7 +92,7 @@ int main() {
     system("cls");
 
     ler_mun(n, m, d);
-
+    busca_mun(n, m, d);
 
     return 0;
 }
